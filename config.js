e55e3a81748a9ba6b860539aefb3d0a198c76431
sad660 <sadahamanupa72@gmail.com>
2026-03-14 09:55:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sad660/PAPPA-QTZ/blob/main/images/luffy-red-glow-eyes-dark-pc-wallpaper-preview.jpg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 PAPPA-QTZ Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
